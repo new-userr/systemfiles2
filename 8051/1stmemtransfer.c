68a8b70f7fb2230bad96c46cf636818b78b1708d
new-userr <168989422+new-userr@users.noreply.github.com>
2024-11-06 22:59:20 +0530

    1st_memtransfer.c

--- 8051/1stmemtransfer.c
+++ 8051/1stmemtransfer.c
@@ -1,9 +1,15 @@
- #include<reg51.h>
- void main(void)
- {
- unsigned char x,y,z;
- x=0x05;
- y=0x03;
- z=x-y;
- P0=z;
- }
+#include<reg51.h>
+void main(void)
+{
+unsigned char x, y, z, m, n, t;
+x=0x04;
+y=0x02;
+z=x+y;
+P0=z;
+m=x-y;
+P1=m;
+n=x*y;
+P2=n;
+t=x/y;
+P3=t;
+}
