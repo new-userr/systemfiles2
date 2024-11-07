0f54f4e2d60466f4feae1c90f7cc02922d55938e
new-userr <168989422+new-userr@users.noreply.github.com>
2024-11-07 12:06:17 +0530

    Update and rename decadecounter.c to decadeplushexcounter.c

---
+++ 8051/decadeplushexcounter.c
@@ -0,0 +1,23 @@
+decade and hex counter-:          #include  <reg51.h>
+
+void delay(); 
+
+void main() {
+    unsigned char i; 
+    while (1) {
+        for (i=0; i<=9; i++) {     // Counts from 0 to 9              // for hex counter i<=15
+            P2 = i;                    // Output the count on P2
+            delay();                   // Delay between counts
+        }
+    }
+}
+
+void delay() {
+    TMOD = 0x01;      // Set Timer 0 in mode 1 (16-bit timer)
+    TL0 = 0x00;       // F6 FOR increse delay
+    TH0 = 0x00;       // Adjust TH0 for a specific delay if needed   //for hex counter TH0=0X01     //66 for increse delay           
+    TR0 = 1;     
+    while (TF0 == 0); 
+    TR0 = 0;     
+    TF0 = 0;   
+}
