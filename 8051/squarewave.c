85d888f2aad6d63572df206d08fec95e3d527844
new-userr <168989422+new-userr@users.noreply.github.com>
2024-11-07 12:29:02 +0530

    Update squarewave.c

--- 8051/squarewave.c
+++ 8051/squarewave.c
@@ -1,36 +1,14 @@
-#include <reg51.h>
-
-sbit wave = P2^0;
-void delay();
-void main()
+#include<reg51.h>
+sbit wave=P2^1;
+void timer0(void) interrupt 1
 {
-	wave=0;
-	while(1)
-	{
-		wave=1;
-		delay();
-		wave=0;
-		delay();
-		
-		
-	
-	}
-	
+wave=~wave;
 }
-
-void delay()
-{
-	unsigned char i;
-	TMOD=0X10;
-	for (i=0;i<14;i++)
-	{ 
-		TH1=0X00;
-		TL1=0X00;
-		TR1=1;
-		while(TF1==0);
-		TF1=0;
-		
-		
-	}
-}
-
+void main(){
+while(1){
+TMOD=0x01;
+TH0=0xFF;
+TL0=0xF9;
+IE=0x82;
+TR0=1;
+}}
